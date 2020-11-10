74e02ea6fdb7525ec95eb55c1cc14ef9699f2c37
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-11-10 12:41:49 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,7 +7,7 @@ function onOpenModal(evt) {
   }
 
   evt.preventDefault(); //отмена перехода по ссылке
-  const imageToShow = `<img src= ${evt.target.dataset.source} alt=${tags}>`;
+  const imageToShow = `<img src=${evt.target.dataset.source} alt=''>`;
   console.log(imageToShow);
   const instance = basicLightbox.create(imageToShow);
   instance.show();
